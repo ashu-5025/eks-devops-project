10170b3e961a301e3fc02760b63a9abb02139845
ashutosh sahu <ashutoshsahu.5025@gmail.com>
2025-08-06 22:34:27 +0530

    vpc and secrets updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.3"
 
   backend "s3" {
-    bucket         = "my-devops-tf-state-bucket"         
+    bucket         = "eks-devops-tf-state-bucket"         
     key            = "eks-infra/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "my-devops-tf-locks"          
+    dynamodb_table = "eks-devops-tf-locks"          
     encrypt        = true
   }
 }
