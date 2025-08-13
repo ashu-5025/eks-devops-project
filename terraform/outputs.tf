f6ea161d0c661b30d062b43c0044e31d32740281
ashutosh sahu <ashutoshsahu.5025@gmail.com>
2025-08-13 23:13:56 +0530

    package_update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,8 @@ output "vpc_id" {
 output "private_subnet_ids" {
   value = module.vpc.private_subnets
 }
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
